f831d06a0e216ab288bdc74786fc332e9ef3f0bf
julinhodavan <lalalulu1wo@gmail.com>
2026-06-25 08:56:34 -0300

    Atualizado cadastro de funcionarios com cargo

--- src/Funcionario.php
+++ src/Funcionario.php
@@ -4,10 +4,12 @@ class Funcionario
 {
     private $nome;
     private $matricula;
+    private $cargo;
 
-    public function __construct($nome, $matricula)
+    public function __construct($nome, $matricula, $cargo)
     {
         $this->nome = $nome;
         $this->matricula = $matricula;
+        $this->cargo = $cargo;
     }
 }
